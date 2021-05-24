3236299a57373025a97287c1c82057ad876be568
Laurence Park <lapark@scem.uws.edu.au>
2021-05-24 16:48:06 +1000

    Used Aaron's date format on the x axis.

--- sydney_corona.R
+++ sydney_corona.R
@@ -20,5 +20,14 @@ dJD <- d %>% filter((t >= "2020-07-01") & (t <= "2020-12-31"))
 
 library("ggplot2")
 g <- ggplot(data = dJD) +
-  geom_line(mapping = aes(x = t, y = H))
+  geom_line(mapping = aes(x = t, y = H)) +
+  scale_x_date(date_labels = "%d %b %Y")
 print(g)
+
+# 1. Adjust the script so that the each month is shown in the x axis.
+# 2.  Commit the new changes (make sure to be providing informative
+# log messages).
+# 3.  Change the plot line to be red and make sure that plot is labelled
+# correctly.
+# 4.  Commit the new changes (make sure to be providing informative
+# log messages).
